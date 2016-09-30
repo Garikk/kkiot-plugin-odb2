6eb30d7e7228c3680a2ef1fab982e2f8c5acfbdc
Garikk <supergarikk@gmail.com>
2016-09-30 20:14:45 +0300

    ///

--- src/main/java/kkdev/kksystem/plugin/odb2/KKPlugin.java
+++ src/main/java/kkdev/kksystem/plugin/odb2/KKPlugin.java
@@ -5,10 +5,12 @@ package kkdev.kksystem.plugin.odb2;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import kkdev.kksystem.base.classes.plugins.PluginConfiguration;
 import kkdev.kksystem.base.classes.plugins.PluginMessage;
 import kkdev.kksystem.base.classes.plugins.simple.KKPluginBase;
 import kkdev.kksystem.plugin.odb2.manager.ODB2Manager;
 import kkdev.kksystem.base.interfaces.IPluginBaseConnection;
+import kkdev.kksystem.plugin.odb2.configuration.PluginSettings;
 
 /**
  *
@@ -31,5 +33,9 @@ public final class KKPlugin extends KKPluginBase {
     public void executePin(PluginMessage Pin) {
         super.executePin(Pin);
        Global.PM.ReceivePin(Pin.FeatureID,Pin.pinName, Pin.getPinData());
+    }
+     @Override
+    public PluginConfiguration getPluginSettings() {
+       return PluginSettings.MainConfiguration;
     }
 }
